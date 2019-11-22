f09a7838e817c3fb19350309b9c777b33e935cd3
orellana12 <48371015+orellana12@users.noreply.github.com>
2019-11-21 22:15:52 -0800

    fixed game

---
+++ Meagm4n new/Assets/Scripts/DragonSeekerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonSeekerController : MonoBehaviour
+{
+    public float speed;
+    public Transform playerDetector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        RaycastHit2D detectorInfo = Physics2D.Raycast(playerDetector.position, Vector2.down);
+        if(detectorInfo.transform.tag == "Player")
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Health health = collision.gameObject.GetComponent<Health>();
+            if(health != null)
+            {
+                health.TakeDamage(20);
+            }
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            explode();
+        }
+
+    }
+
+    void explode()
+    {
+        var collisions = Physics2D.CircleCastAll(gameObject.transform.position, 5, transform.position);
+        foreach(var c in collisions)
+        {
+            if (c.collider.gameObject.CompareTag("Player"))
+            {
+                Health health = c.collider.gameObject.GetComponent<Health>();
+                if (health != null)
+                {
+                    health.TakeDamage(20);
+                }
+            }
+        }
+        Destroy(gameObject);
+    }
+
+}
---
+++ Meagm4n new/Assets/Scripts/JohnsonPlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohnsonPlayerController : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D boxCollider2d;
+    public Health health;
+    JohnsonBar bar;
+    Transform direction;
+
+    bool grounded = true;
+    bool onWall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        health = GetComponent<Health>();
+        direction = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        bar.game.JohnsonHP = health.getHealth();
+        print(bar.game.JohnsonHP.ToString());
+
+        if (Input.GetKey("h"))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            rb.velocity = new Vector2(6, rb.velocity.y);
+        }
+        else if (Input.GetKey("f"))
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            rb.velocity = new Vector2(-6, rb.velocity.y);
+        }
+
+        if (Input.GetKey("t") && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 20);
+            grounded = false;
+        }
+        else if (Input.GetKey("t") && onWall && direction.rotation.y == 0)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            rb.velocity = new Vector2(-6, 20);
+            onWall = false;
+        }
+        else if (Input.GetKey("t") && onWall && direction.rotation.y != 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            rb.velocity = new Vector2(6, 20);
+            onWall = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+            onWall = false;
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            onWall = true;
+            grounded = false;
+
+        }
+    }
+}
